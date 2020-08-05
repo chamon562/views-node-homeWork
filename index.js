daacde0efe03ab2b22b13209c6d59a157d1b58af
Chamon562 <Chamon562@gmail.com>
2020-08-05 12:53:59 -0700

    [sendFile html folders]Add comment out .send and leave .sendFile I mkdir views cd into views touch three folders index.html about.html blog-directory.html created boiler plates for each html inserted h1 in the body and wrote basic info with my name and the name of the file

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 // http://localhost:8000/
 // prints This is the Home Page! on web page
 app.get('/', function(req, res) {
-    res.send('This is the Home Page!')
+    // res.send('This is the Home Page!')
     res.sendFile(__dirname+'/views/index.html');
   });
 
@@ -15,7 +15,7 @@ app.get('/', function(req, res) {
 //when typing in the same local host and /about
 //it displays the message res.sed('Some stuff about me...)
   app.get('/about', function(req, res) {
-      res.send('Some stuff about me is that I dont know what Im doing.')
+    //   res.send('Some stuff about me is that I dont know what Im doing.')
       // res.sendFile(__dirname+'/views/about.html)
     //   res.sendFile( takes an absolute path, so we can't just give it ./views/index.html
     // _dirname is a Node keyword that gives us the absolute path of the current directory (docs), 
@@ -25,7 +25,7 @@ app.get('/', function(req, res) {
   //need route for blog
 //http://localhost:8000/blog displays the res.send('A directory of all my blog post will go here')
   app.get('/blog', function(req, res) {
-      res.send('A directory of all my blog posts will go here.')
+    //   res.send('A directory of all my blog posts will go here.')
     res.sendFile(__dirname+'/views/blog-directory.html');
   });
 
